ce5c4556e88b8c9d18a4cec34ef1a4686ad343d9
garywei944 <33930674+garywei944@users.noreply.github.com>
2020-12-07 10:25:54 -0500

    Finish CameraMove

--- Assets/Sandbox/Gary/CameraMove.cs
+++ Assets/Sandbox/Gary/CameraMove.cs
@@ -8,42 +8,48 @@ namespace Sandbox.Gary
         public float scrollSpeed = 1000f;
         public float space = 50f;
 
+        public float minX = -65;
+        public float maxX = 0;
+        public float minY = 20;
+        public float maxY = 80;
+        public float minZ = -15;
+        public float maxZ = 30;
+
+
         private void Update()
         {
+            var pos = transform.position;
             if (Input.GetKey(KeyCode.A) || Input.mousePosition.x < space)
             {
-                transform.position += Vector3.left * (moveSpeed * Time.deltaTime);
+                pos += Vector3.left * (moveSpeed * Time.deltaTime);
             }
 
             if (Input.GetKey(KeyCode.D) || Input.mousePosition.x > Screen.width - space)
             {
-                transform.position += Vector3.right * (moveSpeed * Time.deltaTime);
+                pos += Vector3.right * (moveSpeed * Time.deltaTime);
             }
 
             if (Input.GetKey(KeyCode.W) || Input.mousePosition.y > Screen.height - space)
             {
-                transform.position += Vector3.forward * (moveSpeed * Time.deltaTime);
+                pos += Vector3.forward * (moveSpeed * Time.deltaTime);
             }
 
             if (Input.GetKey(KeyCode.S) || Input.mousePosition.y < space)
             {
-                transform.position += Vector3.back * (moveSpeed * Time.deltaTime);
+                pos += Vector3.back * (moveSpeed * Time.deltaTime);
             }
 
             var scroll = Input.GetAxis("Mouse ScrollWheel");
             if (scroll != 0)
             {
-                transform.position += Vector3.down * (scroll * scrollSpeed * Time.deltaTime);
-                if (transform.position.y < 20)
-                {
-                    transform.position += Vector3.up * (20 - transform.position.y);
-                }
-
-                if (transform.position.y > 80)
-                {
-                    transform.position += Vector3.up * (80 - transform.position.y);
-                }
+                pos += Vector3.down * (scroll * scrollSpeed * Time.deltaTime);
             }
+
+            // Check position range
+            pos.x = Mathf.Clamp(pos.x, minX, maxX);
+            pos.y = Mathf.Clamp(pos.y, minY, maxY);
+            pos.z = Mathf.Clamp(pos.z, minZ, maxZ);
+            transform.position = pos;
         }
     }
 }
