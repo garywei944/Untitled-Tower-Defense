53c0777aae3b94c8374581300ca9e9ca74875996
15533 <yicao928@gmail.com>
2020-12-09 23:26:00 +0800

    main menu and win page

---
+++ Assets/Scripts/WinPage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WinPage : MonoBehaviour
+{
+    public Button mainMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu.onClick.AddListener(GoToMainMenu);
+    }
+
+    void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+}
