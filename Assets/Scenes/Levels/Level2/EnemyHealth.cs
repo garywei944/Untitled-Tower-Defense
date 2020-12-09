7666b2fe2e67d8707961b3a7304028413b0d31f9
15533 <yicao928@gmail.com>
2020-12-10 01:26:45 +0800

    level win page

---
+++ Assets/Scenes/Levels/Level2/EnemyHealth.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float initHealth = 100f;
+    private float _currentHealth;
+
+    public Image hpBar;
+    public GameObject selfObject;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _currentHealth = initHealth;
+    }
+
+    public void Damage(float amount)
+    { 
+        _currentHealth -= amount;
+        hpBar.fillAmount = _currentHealth / initHealth;
+        if (_currentHealth <= 0)
+        {
+            Debug.Log("die");
+            selfObject.GetComponent<Enemy>().Die();
+        }
+    }
+}
